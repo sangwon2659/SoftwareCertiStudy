ac2d45a3f6c714d886cd3b408ee1904bb6b1e9f8
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2024-02-14 14:17:28 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold V/15686. 치킨 배달/치킨 배달.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct Pos
+{
+    int y;
+    int x;
+};
+
+//ifstream txt_input("input.txt");
+int N, M;
+int num;
+vector<Pos> vectorHouse;
+vector<Pos> vectorChicken;
+vector<Pos> vectorChickenCandidate;
+int resultTotalDist = 100000;
+
+int sumChickenDist()
+{
+    int totalDist = 0;
+    for (auto house : vectorHouse)
+    {
+        int nowDist = 100000;
+        for (auto chicken : vectorChickenCandidate)
+        {
+            int dist = abs(house.y - chicken.y) + abs(house.x - chicken.x);
+            if (dist < nowDist)
+            {
+                nowDist = dist;
+            }
+        }
+
+        totalDist += nowDist;
+    }
+
+    return totalDist;
+}
+
+void Combination(const int& idx)
+{
+    if (vectorChickenCandidate.size() == M)
+    {
+        int nowSumChickenDist = sumChickenDist();
+        if (nowSumChickenDist < resultTotalDist)
+        {
+            resultTotalDist = nowSumChickenDist;
+        }
+        return;
+    }
+
+    for (int i = idx; i < vectorChicken.size(); i++)
+    {
+        vectorChickenCandidate.push_back(vectorChicken[i]);
+        Combination(i + 1);
+        vectorChickenCandidate.erase(vectorChickenCandidate.end() - 1);
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    for (int r = 0; r < N; r++)
+    {
+        for (int c = 0; c < N; c++)
+        {
+            cin >> num;
+            if (num == 1)
+            {
+                vectorHouse.push_back({r, c});
+            }
+            else if (num == 2)
+            {
+                vectorChicken.push_back({r, c});
+            }
+        }
+    }
+
+    Combination(0);
+    cout << resultTotalDist;
+
+    return 0;
+}
