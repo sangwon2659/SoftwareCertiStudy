5303a879358219305535ee11bd7a9a0ebb81866b
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2024-02-14 14:15:53 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver I/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+int N, K;
+int visited[100001];
+
+int computation(int input, int mode)
+{
+	//mode -> [0: -1]  [1: +1]  [2:  *2]
+	if (mode == 0) return input - 1;
+	else if (mode == 1) return input + 1;
+	else if (mode == 2) return 2 * input;
+}
+
+void bfs()
+{
+	queue<int> q;
+	q.push(N);
+	visited[N] = 0;
+
+	while (!q.empty())
+	{
+		int v = q.front();
+		q.pop();
+
+		if (v == K) break;
+
+		for (int i = 0; i < 3; i++)
+		{
+			int result;
+			result = computation(v, i);
+			if (result < 0 || result > 100000) continue;
+			if (visited[result] > 0 || result == N) continue;
+			visited[result] = visited[v] + 1;
+			q.push(result);
+		}
+	}
+}
+
+int main()
+{
+	//ifstream txt_input("input.txt");
+	cin >> N >> K;
+
+	bfs();
+
+	cout << visited[K];
+
+	return 0;
+}
