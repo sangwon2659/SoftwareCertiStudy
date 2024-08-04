d96b59e9237eb8daeb8d8c31de182d62b9ea7586
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2024-08-04 13:16:29 +0900

    [Gold III] Title: 연구소 3, Time: 60 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/17142. 연구소 3/연구소 3.cc
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+struct Pos
+{
+    int y;
+    int x;
+    int depth = 0;
+};
+
+string grid[50][50];
+string tempgrid[50][50];
+
+int N, M;
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+Pos VirusContainer[10];
+Pos CombinationContainer[10];
+int iCount = 0;
+
+int minTime = 100000;
+
+void printMap()
+{
+    for (int r = 0; r < N; r++)
+    {
+        for (int c = 0; c < N; c++)
+        {
+            cout << tempgrid[r][c] << " ";
+        }
+
+        cout << endl;
+    }
+
+    cout << "================" << endl;
+}
+
+bool checkAllSpread()
+{
+    for (int r = 0; r < N; r++)
+    {
+        for (int c = 0; c < N; c++)
+        {
+            if (tempgrid[r][c] == "0") return false;
+        }
+    }
+
+    return true;
+}
+
+void copyGrid()
+{
+    for (int r = 0 ; r < N; r++)
+    {
+        for (int c = 0; c < N; c++)
+        {
+            tempgrid[r][c] = grid[r][c];
+        }
+    }
+}
+
+int spreadVirus()
+{
+    copyGrid();
+    queue<Pos> q;
+    for (int idx = 0; idx < M; idx++)
+    {
+        tempgrid[CombinationContainer[idx].y][CombinationContainer[idx].x] = "*";
+        q.push(CombinationContainer[idx]);
+    }
+    int maxDepth = 0;
+    bool ret = true;
+
+    while(!q.empty())
+    {
+        Pos v = q.front();
+        q.pop();
+
+        if (tempgrid[v.y][v.x] != "0" && tempgrid[v.y][v.x] != "V" && tempgrid[v.y][v.x] != "*") continue;
+
+        if (v.depth > maxDepth && tempgrid[v.y][v.x] != "V")
+        {
+            //printMap();
+            maxDepth = v.depth;
+        }
+
+        if (tempgrid[v.y][v.x] != "*")
+        {
+            tempgrid[v.y][v.x] = to_string(v.depth);
+        }
+
+        if (v.depth > minTime)
+        {
+            ret = false;
+            break;
+        }
+
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int targetY = v.y + dy[dir];
+            int targetX = v.x + dx[dir];
+
+            if (targetY < 0 || targetY >= N) continue;
+            if (targetX < 0 || targetX >= N) continue;
+
+            if ((tempgrid[targetY][targetX] != "0" && tempgrid[targetY][targetX] != "V") ||
+            tempgrid[targetY][targetX] == "X") continue;
+
+            q.push({targetY, targetX, v.depth + 1});
+        }
+    }
+
+    //if (ret) printMap();
+    if (!checkAllSpread()) return -1;
+
+    return maxDepth;
+}
+
+void Solution(const int& nowPos, const int& nowVal)
+{
+    if (nowPos == M)
+    {
+        int result = spreadVirus();
+        //cout << "Result: " << result << endl;
+
+        if (result < minTime && result != -1) minTime = result;
+        return;
+    }
+
+    for (int idx = nowVal; idx < iCount; idx++)
+    {
+        CombinationContainer[nowPos] = VirusContainer[idx];
+        Solution(nowPos + 1, idx + 1);
+    }
+}
+
+int main()
+{
+    //fstream txt_input("input.txt");
+    cin >> N >> M;
+    int a;
+
+    for (int r = 0; r < N; r++)
+    {
+        for (int c = 0; c < N; c++)
+        {
+            cin >> a;
+            if (a == 0) grid[r][c] = "0";
+            else if (a == 1) grid[r][c] = "X";
+            else
+            {
+                grid[r][c] = "V";
+                VirusContainer[iCount] = {r, c, 0};
+                iCount++;
+            }
+        }
+    }
+
+    Solution(0, 0);
+
+    if (minTime != 100000) cout << minTime;
+    else cout << -1;
+
+    return 0;
+}
