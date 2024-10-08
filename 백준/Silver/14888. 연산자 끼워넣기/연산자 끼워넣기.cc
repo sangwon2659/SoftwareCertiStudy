0d0378ef2b15b34e30e98d7d7a7224b53d2c2adb
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2024-10-08 18:27:47 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int N;
+int iMaxResult = -1000000001;
+int iMinResult = 1000000001;
+
+vector<int> vA;
+vector<int> vNumArith;
+vector<int> Container(10);
+
+void Solution(const int& iNowPos)
+{
+	if (iNowPos == N - 1)
+	{
+		int result = vA[0];
+
+		for (int idx = 0; idx < N - 1; idx++)
+		{
+			if (Container[idx] == 0) result += vA[idx + 1];
+			else if (Container[idx] == 1) result -= vA[idx + 1];
+			else if (Container[idx] == 2) result *= vA[idx + 1];
+			else if (Container[idx] == 3)
+			{
+				if (result > 0)
+				{
+					result = (int)(result / vA[idx + 1]);
+				}
+				else
+				{
+					result *= -1;
+					result = (int)(result / vA[idx + 1]);
+					result *= -1;
+				}
+			}
+		}
+
+		if (result > iMaxResult) iMaxResult = result;
+		if (result < iMinResult) iMinResult = result;
+	}
+
+	for (int idx = 0; idx < 4; idx++)
+	{
+		if (vNumArith[idx] <= 0) continue;
+
+		vNumArith[idx]--;
+		Container[iNowPos] = idx;
+		Solution(iNowPos + 1);
+		vNumArith[idx]++;
+	}
+}
+
+int main()
+{
+	//fstream txt_input("input.txt");
+	cin >> N;
+
+	for (int idx = 0; idx < N; idx++)
+	{
+		int A;
+		cin >> A;
+		vA.push_back(A);
+	}
+
+	for (int idx = 0; idx < 4; idx++)
+	{
+		int iNumArith;
+		cin >> iNumArith;
+		vNumArith.push_back(iNumArith);
+	}
+
+	Solution(0);
+
+	cout << iMaxResult << endl;
+	cout << iMinResult << endl;
+
+	return 0;
+}
