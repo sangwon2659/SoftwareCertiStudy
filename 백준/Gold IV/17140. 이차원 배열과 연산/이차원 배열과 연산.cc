18ba128606005b727e5e313ee2243190ec5e90aa
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2024-10-12 13:41:19 +0900

    Update 이차원 배열과 연산.cc

--- 백준/Gold IV/17140. 이차원 배열과 연산/이차원 배열과 연산.cc
+++ 백준/Gold IV/17140. 이차원 배열과 연산/이차원 배열과 연산.cc
@@ -17,6 +17,11 @@ sort(v.begin(), v.end(), [](const Info& P1, const Info& P2)
     if (P1.iFreq == P2.iFreq) return P1.iNum < P2.iNum;
     else return P1.iFreq < P2.iFreq;
 });
+질문게시판 고수님에 의하면
+sort는 원소에 항상 앞에 있음을 보장하지 않아서
+이런 경우에는 stable_sort를 써야 함...(?)
+실제로 이렇게 하니까 맞았음...
+그냥 무조건 if문으로 sort는 한번에 하자
 */
 
 #include <iostream>
